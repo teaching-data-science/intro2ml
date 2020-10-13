0e0f47254097c4d3853c7ead118aa789bf829922
schalkdaniel <d-schalk@t-online.de>
2020-10-13 17:07:37 +0200

    [WIP] fix bug with wrongly returned DOM for answer

--- src/components/choice.js
+++ src/components/choice.js
@@ -4,60 +4,25 @@ import classNames from 'classnames'
 import { Button } from './button'
 import classes from '../styles/choice.module.sass'
 
-//const  asel = []
 const Choice = ({ id = '0', children = [] }) => {
     //var selected_buffer
     const [asel, setAsel] = useState([])
     function updateSelection (asel_tmp, i) {
-      //console.log(">>> FROM CLICK")
-      //console.log("> i:")
-      //console.log(i)
-      //console.log("> selected:")
-      //console.log(selected)
-      //console.log(options)
-      //console.log("")
-      //.setState({test: i})
-      //selected_buffer = i
-      //console.log("asel.includes")
-      //console.log(i)
-      //console.log(asel.includes(i))
-      //console.log("CHANGE")
-      //options[1].props.test = "bla"
-      //{options.map(({ key, props }, i) => {
-         //props.test = i
-      //})}
-      //console.log("> Selection at start of update:")
-      //console.log(asel)
-
       if (asel_tmp.includes(i)) {
         // Remove i from selected items and set selection to undefined:
-        //console.log("> asel includes i!")
         for (var k = 0; k < asel_tmp.length; k++) {
           if (asel_tmp[k] === i) {
             asel_tmp.splice(k, 1)
             setSelected(undefined)
-            //console.log("> Remove i from asel, new asel is:")
-            //console.log(asel)
           }
         }
       } else {
-        //console.log("> asel does not include i")
         asel_tmp.push(i)
-        //console.log("> Include i to asel:")
-        //console.log(asel)
         setSelected(i)
-        //console.log("> Set selected to i")
       }
-      //console.log("selected is")
-      //console.log(selected)
       if (selected === undefined) {
         setSelected(i)
-        //console.log("Update selected from undefined to i")
       }
-      //asel.push(i)
-      //console.log(asel)
-      //console.log(asel.includes(i))
-      //console.log(options)
       return asel_tmp
     }
     function changeUpdate (i) { }
@@ -67,14 +32,13 @@ const Choice = ({ id = '0', children = [] }) => {
     const handleAnswer = useCallback(() => setAnswer(selected), [selected])
     const options = children.filter(child => child !== '\n')
     // Required to prevent the "Submit" button from not reacting due to undefined selected:
-    //const options = children
     {options.map(({ key, props }, i) => {
        const isCorrect = props.correct
        if (isCorrect) { rr_corr.push(i) }
     })}
     const [all_correct, setCorrect] = useState(null)
     function updateAnswer () {
-      setAnswer(1)
+      setAnswer(rr_corr[0])
       var has_equal_length = asel.length === rr_corr.length
       var tmp = true
       if (has_equal_length) {
